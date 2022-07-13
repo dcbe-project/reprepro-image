d916a220e7b05793ac8eb05a69cae5234a21b4da
Manuel Traut <manuel.traut@mt.com>
2022-07-13 11:27:27 +0200

    Publish repo gpg public key
    Signed-off-by: Manuel Traut <manuel.traut@mt.com>

--- start.sh
+++ start.sh
@@ -8,4 +8,6 @@ gpg --import /keys/repo-pub
 mkdir -p /repo/incoming
 chmod 777 /repo/incoming
 
+cat /keys/repo-pub | gpg --dearmor > /repo/dcbe.gpg
+
 inoticoming --foreground /repo/incoming --suffix .changes reprepro -s --basedir /repo processincoming dcbe {} \;
